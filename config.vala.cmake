cf38e4f921e4b742c0d23b129a4daf51193a4883
Giulio Collura <random.cpp@gmail.com>
2011-09-05 15:19:41 +0200

    Added a sample .plug keyfile

--- config.vala.cmake
+++ config.vala.cmake
@@ -1,4 +1,5 @@
 namespace Constants {
+public const string PLUGNAME = "@PLUGNAME@";
 public const string DATADIR = "@DATADIR@";
 public const string PKGDATADIR = "@PKGDATADIR@";
 public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
--- src/config.vala
+++
@@ -1,5 +0,0 @@
-namespace Constants {
-public const string DATADIR = "/usr/lib/plugs";
-public const string PKGDATADIR = "/usr/lib/plugs/pantheon/info-plug";
-public const string GETTEXT_PACKAGE = "info-plug";
-}
